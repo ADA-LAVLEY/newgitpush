cb5c06d331243a38e2b2f18a325af4a9819c1365
ADA-LAVLEY <107742280+ADA-LAVLEY@users.noreply.github.com>
2022-09-10 12:10:20 +0500

    gets

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const std = [20,21,22];
 function plus(std) {
     let son = 0;
     for(let sep of std){
-        if(son += sep /2){ 
+        if(son += sep / 2){ 
            console.log([son]);
         }
 
