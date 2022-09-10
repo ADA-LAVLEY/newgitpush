3175677a49ece73628ca5f02a7005ece7eceaa9d
ADA-LAVLEY <107742280+ADA-LAVLEY@users.noreply.github.com>
2022-09-10 10:56:29 +0500

    masala

---
+++ index.js
@@ -0,0 +1,13 @@
+const std = [20,21,22];
+// plus(std);
+
+function plus(std) {
+    let son = 0;
+    for(let sep of std){
+        if(son += sep /2){ 
+           console.log([son]);
+        }
+
+    }
+}
+plus(std);
